58b60911d1b3684b9d21770e21eacb75b756fdf4
CRUSADER1143 <kai9901@hotmail.com>
2019-09-24 20:11:01 +0800

    第三步

--- WF/src/字母频率统计/test2.java
+++ WF/src/字母频率统计/test2.java
@@ -107,6 +107,58 @@
               System.out.println(key+"出现："+hashMap.get(key)+"次");
           }
       }
+      
+      public static void word2(String args) throws IOException {
+          Scanner input=new Scanner(System.in);
+          System.out.println("输入要统计的个数：");
+          int n=input.nextInt();
+          File file = new File(args);
+          FileInputStream fis = new FileInputStream(file);
+          InputStreamReader isr = new InputStreamReader(fis);
+  
+          int thechar;
+          StringBuffer sb = new StringBuffer();
+          HashMap<String, Integer> wordList = new HashMap<String, Integer>();
+          
+          while ((thechar = isr.read()) != -1) {
+              char letter = (char) thechar;
+              if ((letter >= 'a' && letter <= 'z')
+                      || (letter >= 'A' && letter <= 'Z')) {
+                  sb.append(letter);
+              } else if (sb.length() != 0) {
+                  String theword = new String(sb);
+                  if (wordList.containsKey(theword)) {
+                      wordList.put(theword, wordList.get(theword) + 1);
+                  } else {
+                      wordList.put(theword, 1);
+                  }
+                  sb.delete(0, sb.length());
+              }
+          }
+          isr.close();
+          
+          List<Map.Entry<String, Integer>> words = new ArrayList<Map.Entry<String, Integer>>(
+                 wordList.entrySet());
+         
+          Collections.sort(words, new Comparator<Map.Entry<String, Integer>>() {
+  
+              @Override
+              public int compare(Entry<String, Integer> o1,
+                      Entry<String, Integer> o2) {
+                  return -(o1.getValue() - o2.getValue());
+              }
+          });
+          System.out.println("读取的文件中出现频率最多的"+n+"个单词是：");
+          int i = 0;
+          for (Map.Entry<String, Integer> node : words) {
+              if (i < n) {
+                  System.out.println(node.getKey() + " : " + node.getValue());
+              } else {
+                  break;
+              }
+              i++;
+          }
+      }
      
      
      public static void menu() {
@@ -114,6 +166,7 @@
          System.out.println("欢迎访问本系统");
          System.out.println("1.统计字母出现频率");
          System.out.println("2.统计不重复单词情况");
+         System.out.println("3.统计出现次数前n个单词");
          System.out.println("0.退出");
          System.out.println("*****************************");
          System.out.println("请选择：");
@@ -130,6 +183,7 @@
          {
          case 1:wf(args[1]);break;
          case 2:word1(args[1]);break;
+         case 3:word2(args[1]);break;
          case 0:a=0;break;
          default:a=0;break;
          }
